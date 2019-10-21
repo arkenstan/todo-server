a0de31ac64e8533754dc41beac938c4063818c89
arkenstan <anshulwagadre@gmail.com>
2019-10-21 13:15:05 +0530

    added test cases

--- test/services/client/v1.0/task.test.ts
+++ test/services/client/v1.0/task.test.ts
@@ -1,9 +1,9 @@
 import assert from 'assert';
 import app from '../../../../src/app';
 
-describe('\'client/v1.0/task\' service', () => {
+describe("'client/v1.0/task' service", () => {
   it('registered the service', () => {
-    const service = app.service('clientApi/v1.0/list/:listId/task');
+    const service = app.service('clientApi/v1.0/task');
 
     assert.ok(service, 'Registered the service');
   });
--- test/services/users.test.ts
+++ test/services/users.test.ts
@@ -1,10 +1,26 @@
 import assert from 'assert';
 import app from '../../src/app';
 
-describe('\'users\' service', () => {
-  it('registered the service', () => {
+describe("'users' service", () => {
+  it('registered the service and clear all users', async () => {
     const service = app.service('users');
-
     assert.ok(service, 'Registered the service');
+    await app.service('users')._remove(null);
+  });
+
+  it(`creates a user, encrypt password`, async () => {
+    const user = await app
+      .service('users')
+      .create({ email: 'test@example.com', password: 'superSecret' });
+    // Make sure password got encrypted
+    assert.ok(user.password !== 'superSecret');
+  });
+
+  it(`removes password for external requests`, async () => {
+    const params = { provider: 'rest' };
+    const user = await app
+      .service('users')
+      .create({ email: 'test2@example.com', password: 'superSecret' }, params);
+    assert.ok(!user.password);
   });
 });
