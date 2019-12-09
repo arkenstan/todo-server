6cc89f43a65210e4604c0749f22727c87833fc75
Anshul Wagadre <13753067+arkenstan@users.noreply.github.com>
2019-12-09 21:54:10 +0530

    Revert "Feature 1"

--- test/services/task.test.ts
+++ test/services/task.test.ts
@@ -1,23 +1,10 @@
 import assert from 'assert';
 import app from '../../src/app';
 
-describe("'task' service", () => {
+describe('\'task\' service', () => {
   it('registered the service', () => {
     const service = app.service('task');
+
     assert.ok(service, 'Registered the service');
-  });
-
-  it('creates and processes task, adds user information', async () => {
-    const user = await app
-      .service('users')
-      .create({ email: 'createTaskUser@example.com', password: 'supersecret' });
-    const params = { user };
-    const task = await app
-      .service('task')
-      .create({ content: 'Task 1', additional: 'should be removed' }, params);
-
-    assert.equal(task.content, 'Task 1');
-    assert.equal(task.createdBy, user._id);
-    assert.ok(!task.additional);
   });
 });
